139bf8ebc614cb0e667d08da32cceac716ba5ba5
Akriti Gupta <79895428+akriti890700@users.noreply.github.com>
2021-03-02 19:20:29 +0530

    Add files via upload

---
+++ Moq/Customer/CustomerComm.Tests/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomerCommLib;
+using NUnit.Framework;
+using Moq;
+
+namespace CustomerCommTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        Mock<IMailSender> mockMailSender;
+
+        CustomerComm custTest;
+
+        [Test]
+        public void TestMethod()
+        {
+            mockMailSender = new Mock<IMailSender>();
+            mockMailSender.Setup(x => x.SendMail("cust123@abc.com", "Some Message")).Returns(true);
+
+            custTest = new CustomerComm(mockMailSender.Object);
+            var result = custTest.SendMailToCustomer();
+
+            Assert.AreEqual(true,result);
+        }
+    }
+}
---
+++ Moq/Player/PlayerManagerTests/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+using PlayersManagerLib;
+
+namespace PlayerManagerTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        Mock<IPlayerMapper> mockPlayerMapper;
+        [OneTimeSetUp]
+        public void init()
+        {
+            mockPlayerMapper = new Mock<IPlayerMapper>();
+            mockPlayerMapper.Setup(s => s.IsPlayerNameExistsInDb(It.IsAny<string>())).Returns(false);
+        }
+        
+        [Test]
+        [TestCase("King")]
+        [TestCase("Kohli")]
+        public void TestPlayer(string name)
+        {  
+            Player p = Player.RegisterNewPlayer(name, mockPlayerMapper.Object);
+            Assert.That(name, Is.EqualTo(p.Name));
+            Assert.That(23, Is.EqualTo(p.Age));
+            Assert.That("India", Is.EqualTo(p.Country));
+            Assert.That(30, Is.EqualTo(p.NoOfMatches));
+
+        }
+    }
+}
