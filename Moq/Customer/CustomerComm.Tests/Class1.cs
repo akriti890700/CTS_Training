3e6061b95995e714098e2832ba2c2d2c2eda3df8
Akriti Gupta <79895428+akriti890700@users.noreply.github.com>
2021-03-03 14:05:53 +0530

    Add files via upload

---
+++ Moq/Customer/CustomerComm.Tests/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomerCommLib;
+using NUnit.Framework;
+using Moq;
+
+namespace CustomerCommTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        Mock<IMailSender> mockMailSender;
+
+        CustomerComm custTest;
+        [OneTimeSetUp]
+        public void init()
+        {
+            mockMailSender = new Mock<IMailSender>();
+            mockMailSender.Setup(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+        }
+
+        [Test]
+        public void TestMethod()
+        {   
+            custTest = new CustomerComm(mockMailSender.Object);
+            var result = custTest.SendMailToCustomer();
+
+            Assert.AreEqual(true,result);
+        }
+    }
+}
